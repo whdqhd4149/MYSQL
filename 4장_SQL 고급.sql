4807b57e728391d9209feb2b8f1e69674fdaaa44
whdqhd4149 <whdqhd4149@gmail.com>
2025-08-10 21:17:18 +0900

    add

--- 4장_SQL 고급.sql
+++ 4장_SQL 고급.sql
@@ -86,6 +86,7 @@ INSERT INTO Sales (uid, year, month, sale) VALUES ('a104', 2020, 2,  84000);
 INSERT INTO Sales (uid, year, month, sale) VALUES ('a105', 2020, 2, 180000);
 INSERT INTO Sales (uid, year, month, sale) VALUES ('a108', 2020, 2,  76000);
 
+use studydb;
 select * from member where name='김유신';
 select * from member where pos='차장' and dep=101;
 select * from member where pos='차장' or dep=101;
@@ -151,4 +152,109 @@ select sum(sale) as 총합, avg(sale) as 평균  from sales where month=2 order 
 -- 실습 4-9
 select min(sale) as 최저, max(sale) as 최고 from sales where year=2020;
 -- 실습 4-10
+select * from sales group by uid;
+select * from sales group by year;
+select * from sales group by uid, year;
+select uid, count(*) as 건수 from sales group by uid;
+select uid, sum(sale) as 합계 from sales group by uid;
+select uid, avg(sale) as 평균 from sales group by uid;
+select uid, year, sum(sale) as 합계
+from sales
+group by uid, year;
+select uid, year, sum(sale) as 합계
+from sales
+group by uid, year
+order by year asc, 합계 desc;
+select uid, year, sum(sale) as 합계
+from sales
+where sale >= 50000
+group by uid, year
+order by 합계 desc;
+-- 실습 4-11
+select uid, sum(sale) as 합계 from sales
+group by uid
+having 합계 >= 200000;
+select uid, year, sum(sale) as 합계
+from sales
+where sale >= 100000
+group by uid, year
+having 합계 >= 200000
+order by 합계 desc;
+-- 실습 4-12
+create table sales2 like sales;
+insert into sales2 select * from sales;
+update sales2 set year = year + 3;
+
+select * from sales union select * from sales2;
+select * from sales where sale >= 100000
+union
+select * from sales2 where sale >= 100000;
+select uid, year, sale from sales
+union
+select uid, year, sale from sales2;
+select uid, year, sum(sale) as 합계
+from sales
+group by uid, year
+union
+select uid, year, sum(sale) as 합계
+from sales2
+group by uid, year
+order by year asc, 합계 desc;
+-- 실습 4-13
+select * from sales inner join member on sales.uid = member.uid;
+select * from member inner join departmemt on member.dep = departmemt.depNo;
+
+select * from sales as a join member as b on a.uid = b.uid;
+select * from member as a join department as b on a.dep= b.depNo;
+
+select * from sales as a, member as b where a.uid = b.uid;
+select * from member as a, department as b where a.dep = b.depno;
+
+select a.seq, a.uid, sale, name, pos from sales as a
+join member as b on a.uid = b.uid;
+
+select a.seq, a.uid, sale, name, pos from sales as a
+join member as b using (uid);
+
+select a.seq, a.uid, sale, name, pos from sales as a 
+join member as b on a.uid = b.uid
+where sale >= 100000;
+
+select  a.uid, a.year, sum(sale) as 합계 from sales as a 
+join member as b on a.uid = b.uid
+group by a.uid, a.year having 합계 >= 100000
+order by a.year asc, 합계 desc;
+
+select * from sales as a 
+join member as b on a.uid = b.uid
+join department as c on b.dep = c.depno;
+
+select a.seq, a.uid, a.sale, b.name, b.pos, c.name from sales as a
+join member as b on a.uid = b.uid
+join department as c on b.dep = c.depno
+where sale > 100000
+order by sale desc;
+
+-- 실습 4-14
+select * from sales as a left join member as b on a.uid = b.uid;
+select * from sales as a right join member as b on a.uid = b.uid;
+
+-- 실습 4-15
+select a.uid, a.name, a.pos, b.name from member as a left join department as b on a.dep = b.depno;
+
+-- 실습 4-16
+select sum(sale) as 매출의_합 from sales as a 
+join member as b on a.uid = b.uid
+where b.name = '김유신' and a.year = 2019;
+
+-- 실습 4-17
+select b.name as 직원이름, c.name as 부서명, b.pos as 직급, a.year as 년도, sum(a.sale) as 매출합 from sales as a
+join member as b on a.uid = b.uid
+join department as c on b.dep = c.depno
+where a.sale >= 50000 and a.year = 2019
+group by b.name, c.name, b.pos, a.year
+having sum(a.sale) >= 100000
+order by 매출합 desc;
+
+
 
